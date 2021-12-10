ecf4c9cdf32380485c0d9acfc6948cb2af327ef6
aliasghar2021 <95918652+aliasghar2021@users.noreply.github.com>
2021-12-10 09:01:15 -0800

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -1,2 +1,12 @@
 import pygame
 import time
+width = 1000
+height = 800
+gameDisplay = pygame.display.set_mode ( ( width , height ) )
+pygame.display.set_caption ( 'Game Ball' )
+clock = pygame.time.Clock ()
+def game_loop () :
+  for event in pygame.event.get () :
+    if event.type == pygame.QUIT () :
+      pygame.quit ()
+      quit ()
